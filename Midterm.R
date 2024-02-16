2bdcf2a36b081d74be0ee7a628c99d76a3e727ab
Egarc56 <158229015+Egarc56@users.noreply.github.com>
2024-02-16 15:42:06 -0500

    Midterm

--- Midterm.R
+++ Midterm.R
@@ -1,18 +1,17 @@
-#Primeramente configuramos el directorio de trabajo en la carpeta que contiene nuestros datos
+#First we configure the working directory in the folder that contains our data
 setwd("Users/MariaGarcia/Downloads/Bioinformatics Eli")
-#Consiguientemente le damos nombre a nuestra sequencias que ya se encuentran agrupadas. En este caso opte por "Homo" ya que nuestras sequencias corresponden a una poblacion de 20 humanos.
+#Then I named the sequences that are already grouped. In this case I opted for "Homos" since the sequences correspond to a sample of 20 humans.
 Homos <- readDNAStringSet("sequences.fasta Midterm.fasta")
-Mutant <-readDNAStringSet("seqdump.txt")
-#Ahora aplicamos msa y asigmanos la nueva valiable (opte por la misma)
+#Now we apply msa and assign us the new varidable (opt for the same one)
 Homos <- msa(Homos)
-#Con esta siguiente funcion podemos determinar la longitud de nuestra sequencia (resultado = 642)
+#With this following function we can determine the length of our sequence (result = 642)
 nchar(Homos)
-#Utilice esta funciones para observar mi alineamiento y asi poder completar el paso 2 de la tarea 
+#Use these functions to observe my alignment so I can complete step 2 of the task
 print(Homos, show="complete")
 alFreq <- alphabetFrequency(Homos)
 alFreq
-# Homo_sapiens 6  es el sujeto que parece presentar la mayoria de mutaciones en los que los nucleotidos han sido reeplazados en varias localizaciones a traves del alineamiento.
-#ran Blast for Homo 6 - Homo sapiens mutant hemoglobin beta chain (HBB) gene 
+# Homo_sapiens 6 is the subject that appears to present the majority of mutations in which nucleotides have been replaced in various locations throughout the alignment.
+#ran Blast for Homo 6 - Homo sapiens mutant hemoglobin beta chain (HBB) gene
 #Accession number-AY356351.1
 #Here I am translating the Homo 6 sequence into protein
 Mutant <-("seqdump.txt")
@@ -24,4 +23,5 @@ write.fasta(names="Mutantx", sequences=Mutantx, file.out="Mutant.fasta")
 # I ran Blastp to search the best match for my protein sequence 
 #result: hemoglobin subunit beta isoform X1 [Mandrillus leucophaeus]
 #Accession Number-XP_011830555.1
-#Afte doing some research I found using GenBank that this gene is associated with sickle beta thalassaemia
+#After doing some research I found using GenBank that this gene is associated with sickle beta thalassaemia. Yes this individual has this genetic disease
+

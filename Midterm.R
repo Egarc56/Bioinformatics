4961bae29062a0714fd24c486a7c5b777302ca20
henicorhina <ojohnson@utep.edu>
2024-02-23 15:00:58 -0500

    update Midterm.R
    Good job overall. A few points:
    Make sure to load libraries at the start of each script
    rename new variables so that you don't erase the previous one
    Put all data files in the same Bioinformatics folder
    30/35

--- Midterm.R
+++ Midterm.R
@@ -1,14 +1,19 @@
+# load libraries
+library(msa)
+library(Biostrings)
+library(seqinr)
+
 #First we configure the working directory in the folder that contains our data
 setwd("/Users/MariaGarcia/Downloads/Bioinformatics Eli")
 #Then I named the sequences that are already grouped. In this case I opted for "Homos" since the sequences correspond to a sample of 20 humans.
 Homos <- readDNAStringSet("sequences.fasta Midterm.fasta")
 #Now we apply msa and assign the new variable (opted for the same name)
-Homos <- msa(Homos)
+Homos.aln <- msa(Homos) #rename new variables so that you don't erase the previous one
 #With this following function we can determine the length of our sequence (result = 642)
-nchar(Homos)
+nchar(Homos.aln)
 #Use these functions to observe my alignment so I can complete step 2 of the Homework
-print(Homos, show="complete")
-alFreq <- alphabetFrequency(Homos)
+print(Homos.aln, show="complete")
+alFreq <- alphabetFrequency(Homos.aln)
 alFreq
 #Homo_sapiens 6 is the subject that appears to present mutations. Nucleotides have been replaced in various locations throughout the alignment.
 #I ran Blast for Homo 6 
@@ -16,11 +21,16 @@ alFreq
 #Accession number-AY356351.1
 #Here I am translating the Homo 6 sequence into protein
 setwd("/Users/MariaGarcia/Downloads")
-Mutant6 <- readDNAStringSet("Homosapien.fasta")
+Mutant6 <- readDNAStringSet("Homosapien.fasta") # file does not exist in the Bioinformatics folder, so I can't access it
+
+# another way to access that sample:
+Mutant6 <- Homos$Homo_sapiens_6
+
 MutantGene6 <- Biostrings::translate(Mutant6)
 print(MutantGene6)
 #Here I am printing the translated sequence into a fasta file using the write.fasta from the seqinr package which i saved to my Bioinformatics folder
-write.fasta(sequences = MutantGene6, names = MutantGene6, file.out = "MutantHomoX")
+write.fasta(sequences = MutantGene6, names = MutantGene6, file.out = "MutantHomoX.fasta") #make sure to provide file extension
 # I used Blast and GenBank to search for the genetic diseases linked to this gene 
-#After doing some research I found that this gene is associated with sickle beta thalassaemia and sickle cell anemia. Yes, this individual has this genetic diseases
+#After doing some research I found that this gene is associated with sickle beta 
+# thalassaemia and sickle cell anemia. Yes, this individual has this genetic diseases
 
